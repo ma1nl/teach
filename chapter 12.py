9e84e88606fc14b7adbbccf6bf0c616edf42cb26
larindima78@gmail.com <larindima78@gmail.com>
2024-04-17 22:57:01 +0300

    тестовый

--- chapter 12.py
+++ chapter 12.py
@@ -21,10 +21,50 @@
 # print_fio(name, surname, patronymic)
 
 
-def print_digit_sum(num: int):
-    num = [int(i) for i in str(num)]
-    summa = sum(num)
-    print(summa)
+# def print_digit_sum(num: int):
+#     num = [int(i) for i in str(num)]
+#     summa = sum(num)
+#     print(summa)
 
-n = int(input())
-print_digit_sum(n)
+# n = int(input())
+# print_digit_sum(n)
+
+
+# def find_all(target: str, symbol):
+#     index_current_symbol = []
+#     for i in range(len(target)):
+#         if target[i] == symbol:
+#             index_current_symbol.append(i)
+#     return index_current_symbol
+
+# s = input()
+# char = input()
+
+# print(find_all(s, char))
+
+
+# def merge(list1, list2):
+#     global_list = list1 + list2
+#     global_list.sort()
+#     return global_list
+
+# numbers1 = [int(c) for c in input().split()]
+# numbers2 = [int(c) for c in input().split()]
+
+# print(merge(numbers1, numbers2))
+
+
+# def merge_list(num_string: int):
+#     global_string = []
+
+#     for _ in range(num_string):
+
+#         temporary_list = [int(j) for j in input().split()]
+
+#     return global_string
+
+# num = int(input())
+# print(merge_list(num))
+
+
+print(1)
