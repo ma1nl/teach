7facf8038ff9d9e6fa2765cfb6f547d5213776cb
larindima78@gmail.com <larindima78@gmail.com>
2024-05-06 15:15:16 +0300

    become new part of course

---
+++ advanced/chapter 1.py
@@ -0,0 +1,17 @@
+# weight, lenght = float(input()), float(input())
+# imt = weight / (lenght * lenght)
+# if 18.5 > imt:
+#     print('Недостаточная масса')
+# elif 18.5 <= imt <= 25:
+#     print('Оптимальная масса')
+# elif imt > 25:
+#     print('Избыточная масса')
+
+# string = input()
+# l_string = len(string)
+# total_cost = l_string * 60
+# rubles = total_cost // 100
+# pennies = total_cost % 100
+# print(f'{rubles} р. {pennies} коп.')
+
+print(len([i for i in input().split()]))
