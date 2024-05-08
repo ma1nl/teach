16350cce7051873553c53ea2161c512c64573b57
larindima78@gmail.com <larindima78@gmail.com>
2024-05-08 19:34:31 +0300

    Fix while, add new strings

--- micro projects/guess_number.py
+++ micro projects/guess_number.py
@@ -2,19 +2,19 @@ import random
 import simple_colors
 
 def repeatable_input(num: int) -> int:
-    '''Пока введенное число не будет находится в диапазоне чисел, будет просить ввести число'''
+    '''Пока введенное число не будет находится в диапозоне чисел, будет просить ввести число'''
     number = input((simple_colors.yellow('Введите число: ')))
     while not is_valid_num(number, num):
         number = input(simple_colors.red(f'А может быть все-таки введем число в диапазоне от 1 до {num}?: '))
     return int(number)
 
 def is_valid_num(number: str, num: int) -> bool:
-    '''Если введенном числе нету символов и букв возращает True или False'''
+    '''Если является числом в диапозоне, то возращает True, иначе False'''
     number = number.strip()
-    if number.isdigit() or number.isdecimal() == False:
-        return 1 <= int(number) <= num
-    else:
+    if not number.isdigit():
         return False
+    
+    return 1 <= int(number) <= num
 
 def is_valid_word(result: str) -> bool:
     '''Проверяет, корректно ли введено слово'''
@@ -42,15 +42,15 @@ def game():
     required_number = random.randint(1, num)
     print()
     print(simple_colors.magenta('Игра началась!'))
-    number = repeatable_input(num)
-    tries += 1
-    while number != required_number:
+    while True:
+        tries += 1
+        number = repeatable_input(num)
         if number > required_number:
             print(simple_colors.cyan('Твоё число больше загаданного, попробуй еще разок'))
         elif number < required_number:
             print(simple_colors.blue('Твоё число меньше загаданного, попробуй еще разок'))
-        tries += 1
-        number = repeatable_input(num)
+        else:
+            break
     print(simple_colors.magenta(f'\x1B[1;3mПобеда!\x1B[0m \n\x1B[3mКоличество попыток: {tries}.\x1B[0m'))
 
 def restart():
@@ -58,7 +58,7 @@ def restart():
     while not is_valid_word(result):
         print(simple_colors.red('Я тебя непонял, повтори'))
         result = input(simple_colors.yellow('Хочешь еще раз?: '))
-    if is_valid_word() and result == 'да':
+    if is_valid_word(result) and result == 'да':
         game()
     else:
         print(simple_colors.magenta('\x1B[1;3mУдачи!\x1B[0m ') + simple_colors.magenta('\x1B[3mУвидимся\x1B[0m ') + simple_colors.magenta('\x1B[1mв другой раз.\x1B[0m'))
