9fffe02aa53c47eac85dd780965a4c22e81e54cb
KanVull <minirorus2@gmail.com>
2024-05-16 23:29:28 +0400

    Add nice words for best fucking gaming

--- micro projects/scrape_site_skanvordov.py
+++ micro projects/scrape_site_skanvordov.py
@@ -1,27 +1,31 @@
-import requests
-from bs4 import BeautifulSoup as bs
+# import requests
+# from bs4 import BeautifulSoup as bs
 
-start_site = '/сканворд/слово-из-5-букв/'
+# start_site = '/сканворд/слово-из-5-букв/'
 
-all_words = []
-
-
-def get_words_from(site):
-    site = 'https://vfrsute.ru' + site
-    html = requests.get(site).text
-    soup = bs(html, 'html.parser')
-    word_groups = soup.find_all('div', class_='words_group')
-    for group in word_groups:
-        more = group.find_all('a', class_=False)
-        if more:
-            get_words_from(more[0]["href"])
-        else:
-            word_links = group.find_all('a', class_='word-link')
-            words = [link['href'].split('=')[-1].replace('/', '') for link in word_links]
-            all_words.extend(words)
+# all_words = []
 
 
-get_words_from(start_site)
+# def get_words_from(site):
+#     site = 'https://vfrsute.ru' + site
+#     html = requests.get(site).text
+#     soup = bs(html, 'html.parser')
+#     word_groups = soup.find_all('div', class_='words_group')
+#     for group in word_groups:
+#         more = group.find_all('a', class_=False)
+#         if more:
+#             get_words_from(more[0]["href"])
+#         else:
+#             word_links = group.find_all('a', class_='word-link')
+#             words = [link['href'].split('=')[-1].replace('/', '') for link in word_links]
+#             all_words.extend(words)
+
+
+# get_words_from(start_site)
+with open('russian.txt', 'r', encoding='utf-8') as f:
+    words = [word.lower().replace('\n', '').replace('ё', 'е') for word in f.readlines()]
+    words.sort()
+
 with open('russian.txt', 'w', encoding='utf-8') as f:
-    for word in all_words:
-        f.write(word + '\n')
+    for word in words:
+        f.write(word + "\n")
--- micro projects/woordly.py
+++ micro projects/woordly.py
@@ -1,6 +1,31 @@
 import random
 
 
+class Word:
+    def __init__(self, file_path) -> None:
+        self.words = self._load_words_from(file_path)
+
+    def _load_words_from(self, file_path: str) -> list:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return [word.replace('\n', '').replace('ё', 'е') for word in f.readlines()]
+
+    def get_random_word(self) -> str:
+        return random.choice(self.words)
+
+    def exists(self, word: str) -> bool:
+        left_bound = 0
+        right_bound = len(self.words) - 1
+        while left_bound <= right_bound:
+            middle = (right_bound + left_bound) // 2
+            if word == self.words[middle]:
+                return True
+            elif word > self.words[middle]:
+                left_bound = middle + 1
+            else:
+                right_bound = middle - 1
+        return False
+
+
 def random_word():
     with open('russian.txt', 'r', encoding='utf-8') as f:
         return random.choice(f.readlines()).replace('\n', '').replace('ё', 'e')
@@ -18,7 +43,26 @@ def print_keyboard(unavailable_letters=[]):
 
 
 def main():
-    print_keyboard()
+    words = Word('russian.txt')
+    word_to_find = words.get_random_word()
+    left_word = 'ааааа'
+    right_word = 'яяяяя'
+    while True:
+        word = input().lower()
+        if words.exists(word):
+            if word > word_to_find:
+                if word < right_word:
+                    right_word = word
+                print(left_word + '..' + right_word)
+            elif word < word_to_find:
+                if word > left_word:
+                    left_word = word
+                print(left_word + '..' + right_word)
+            else:
+                print('vse')
+                break
+        else:
+            print('Ne suschestvuet')
 
 
 if __name__ == '__main__':
