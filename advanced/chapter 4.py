41a24bfe44b0c8efa0d796272731ef2cc728266b
ma1nl <larindima78@gmail.com>
2024-05-27 22:32:07 +0300

    complete some tasks

--- advanced/chapter 4.py
+++ advanced/chapter 4.py
@@ -34,3 +34,41 @@
 #     total += sum(sub_list)
 #     counter +=len(sub_list)
 # print(total / counter)
+
+# right_side = int(input())
+# list_num = []
+# for i in range(1, right_side + 1):
+#     sub_list = [int(k) for k in range(1, i + 1)]
+#     list_num.append(sub_list)
+# for i in range(right_side):
+#     print(list_num[i])
+
+# n = int(input())
+# lst = [[1]]
+# for i in range(1, n + 1):
+#     row = [1] * (i + 1)
+#     for j in range(i + 1):
+#         if j != 0 and j != i:
+#             row[j] = lst[i - 1][j - 1] + lst[i - 1][j]
+#     lst.append(row)
+# print(lst[-1])
+
+# n = int(input())
+# lst = [[1]]
+# for i in range(1, n + 1):
+#     row = [1] * (i + 1)
+#     for j in range(i + 1):
+#         if j != 0 and j != i:
+#             row[j] = lst[i - 1][j - 1] + lst[i - 1][j]
+#     lst.append(row)
+# for row in lst[:-1]:
+#     print(*row)
+
+# text = input().split()
+# lst_sblst = [[text[0]]]
+# for i in range(1, len(text)):
+#     if text[i] == text[i - 1]:
+#         lst_sblst[-1].append(text[i])
+#     else:
+#         lst_sblst.append([text[i]])
+# print(lst_sblst)
